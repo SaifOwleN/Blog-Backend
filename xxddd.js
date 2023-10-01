278c0b3216f16f4c6db4f5317f604406e8e62167
Saif Al-Din Samy <SeifOwleN@proton.me>
2023-10-02 02:33:11 +0300

    xdd

--- xxddd.js
+++
@@ -1,9 +0,0 @@
-let string = 'ahmed'
-const reverse = (string) => {
-    return string
-      .split('')
-      .reverse()
-      .join('')
-}
-reverse(string)
-console.log('string', reverse(string))
