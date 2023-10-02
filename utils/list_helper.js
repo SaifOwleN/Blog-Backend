9e09232ca7aa4c97b75d080e9d9419aa6c9f4262
Saif Al-Din Samy <SeifOwleN@proton.me>
2023-10-02 20:55:01 +0300

    uibuild

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,18 @@
 const dummy = (blogs) => {
   return 1;
 }
-module.exports = {dummy}
+
+const totalLikes = (blogs) => {
+  let total = 0
+  let x = 0
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > total) {
+    total = blogs[i].likes,
+    x = i
+    }
+  };
+
+  return blogs[x]
+}
+    module.exports = {dummy,totalLikes}
+
